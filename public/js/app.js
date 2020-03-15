fa02d54f82b80191a513c9da5ffeb055bafbef4e
venkatharid <venkatharid1990@gmail.com>
2020-03-15 10:29:44 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+const weatherForm = document.querySelector('form');
+const Search = document.querySelector('input');
+const message1= document.querySelector('#message_1');
+const message2= document.querySelector('#message_2');
+
+weatherForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+const location= Search.value;
+message1.textContent= "Loading";
+message2.textContent="";
+fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+        message1.textContent=data.error;
+        }else{
+        message1.textContent=data.location;
+        message2.textContent=data.forecastData;
+        }
+    })
+})
+})
---
+++ src/app.js
@@ -0,0 +1,90 @@
+const path=require('path')
+const express= require('express')
+const hbs= require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+var app = express();
+
+//Define paths for Express config
+var pathtopublic = path.join(__dirname,'../public');
+var veiwPath = path.join(__dirname,'../templates/views');
+var partialPath = path.join(__dirname,'../templates/partials');
+
+
+
+//setup handlebars engine and views location
+app.set("view engine",'hbs')
+app.set('views',veiwPath);
+hbs.registerPartials(partialPath)
+
+app.use(express.static(pathtopublic))
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Weather',
+        name: 'venkat'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'About Me',
+        name: 'Venkat'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title: 'Help',
+        name: 'Venkat'
+})
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'Please provide address'
+        })
+    }
+    geocode(req.query.address, (error, {latitude,longitude,location}={}) => {
+        if (error) {
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error
+                })
+            }
+
+            res.send({
+                address: req.query.address,
+                location,
+                forecastData: forecastData
+            })
+        })
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('error',{
+        title: '404 Error',
+        name:'Venkat',
+        errorMessage:'Help article not found'
+    })
+})
+
+app.get('/*',(req,res)=>{
+    res.render('error',{
+        title: '404 Error',
+        name:'Venkat',
+        errorMessage:'Page not found'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("server is started")
+})
